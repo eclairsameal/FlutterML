3c2e880c29c6cccf2d0009a3f8c6d3cb9fbe9a1a
Huang Peiwen <peiwen.huang@bric.jp>
2023-05-17 02:07:38 +0900

    即時顯示鏡頭

--- live_camera/lib/main.dart
+++ live_camera/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
+late List<CameraDescription> _cameras; // 儲存相機詳細資訊
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras(); // 取得將存儲該設備中存在的每個攝像頭的詳細信息
   runApp(const MyApp());
 }
 
@@ -41,10 +45,38 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  late CameraController cameraController;  // 相機控制器
+  @override
+  void initState(){
+    super.initState();
+    cameraController = CameraController(_cameras[0], ResolutionPreset.high);
+    // description -> 調用哪個鏡頭[0：後鏡頭, 1：前鏡頭]
+    // resolutionPreset -> 分辨率 質量(ResolutionPreset中有很多選擇)
+    cameraController.initialize().then((_) {
+      if (!mounted) { // 無法訪問鏡頭
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+          // Handle access errors here.(用戶沒有授權)
+          print("User denied camera access.");
+            break;
+          default:
+          // Handle other errors here.
+            print("Handle other errors.");
+            break;
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: CameraPreview(cameraController),
 
     );
   }
